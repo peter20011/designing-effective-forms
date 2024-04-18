4c26f4114b53be52f1495b43f17e1967a021520c
peter20011 <piotr.warcholek@student.pk.edu.pl>
2024-04-18 17:27:47 +0200

    added

--- script.js
+++ script.js
@@ -18,28 +18,76 @@ async function fetchAndFillCountries() {
         }
         const data = await response.json();
         const countries = data.map(country => country.name.common);
-        countryInput.innerHTML = countries.map(country => `<option value="${country}">${country}</option>`).join('');
+        countries.sort((a, b) => a.localeCompare(b));
+
+        const datalist = document.createElement('datalist');
+        datalist.id = 'countrySuggestions';
+
+        countries.forEach(country => {
+            const option = document.createElement('option');
+            option.value = country;
+            datalist.appendChild(option);
+        });
+
+        document.body.appendChild(datalist);
+
+        countryInput.addEventListener("input", () => {
+            const search = countryInput.value.toLowerCase();
+            const suggestions = countries.filter(country => country.toLowerCase().startsWith(search));
+
+            while (datalist.firstChild) {
+                datalist.removeChild(datalist.firstChild);
+            }
+
+            suggestions.forEach(suggestion => {
+                const option = document.createElement('option');
+                option.value = suggestion;
+                datalist.appendChild(option);
+            });
+        });
+
+        countryInput.setAttribute('list', 'countrySuggestions');
     } catch (error) {
         console.error('Wystąpił błąd:', error);
     }
 }
 
-function getCountryByIP() {
-    fetch('https://get.geojs.io/v1/ip/geo.json')
+const updateCountryByIP = async () => {
+    await fetch('https://get.geojs.io/v1/ip/geo.json')
         .then(response => response.json())
         .then(data => {
+            console.log(data);
             const country = data.country;
-            // TODO inject country to form and call getCountryCode(country) function
+            countryInput.value = country;
+
+            addCountryCodeToVATNumber(data.country_code);
+            selectVATIfEU(data.continent_code);
         })
         .catch(error => {
             console.error('Błąd pobierania danych z serwera GeoJS:', error);
         });
 }
 
-function getCountryCode(countryName) {
+const selectVATIfEU = (continentCode) => {
+    const vatUECheckbox = document.querySelector('#vatUE');
+
+    if (continentCode === "EU") {
+        vatUECheckbox.click();
+    }
+}
+
+const addCountryCodeToVATNumber = (countryCode) => {
+    console.log(countryCode);
+    const vatNumberField = document.querySelector('#vatNumber');
+    vatNumberField.value += countryCode;
+}
+
+const updateCountryCode = async () => {
+    const countryCodeSelect = document.querySelector('#countryCode');
+    const countryName = countryInput.value;
     const apiUrl = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
 
-    fetch(apiUrl)
+    await fetch(apiUrl)
     .then(response => {
         if (!response.ok) {
             throw new Error('Błąd pobierania danych');
@@ -47,18 +95,52 @@ function getCountryCode(countryName) {
         return response.json();
     })
     .then(data => {        
-        const countryCode = data[0].idd.root + data[0].idd.suffixes.join("")
-        // TODO inject countryCode to form
+        const countryCode = data[0].idd.root + data[0].idd.suffixes.join("");
+
+        Array.from(countryCodeSelect.options).forEach((option) => {
+            if (option.value === countryCode) {
+                countryCodeSelect.value = countryCode;
+            }
+        });
     })
     .catch(error => {
         console.error('Wystąpił błąd:', error);
     });
 }
 
+const addListenersToButtons = (buttons, methodValue) => {
+    buttons.forEach((button) => {
+        button.addEventListener('click', function() {
+            buttons.forEach((btn) => {
+                btn.classList.remove('btn-primary');
+                btn.classList.add('btn-outline-primary');
+            });
 
-(() => {
-    // nasłuchiwania na zdarzenie kliknięcia myszką
+            this.classList.add('btn-primary');
+            this.classList.remove('btn-outline-primary');
+        
+            let selectedMethod = this.getAttribute('data-value');
+            methodValue.value = selectedMethod;
+        });
+    });
+}
+
+const initializeButtonListeners = () => {
+    const shippingMethodButtons = document.querySelectorAll('.shipping-field');
+    const shippingMethodValue = document.querySelector('#shippingMethod');
+    const paymentMethodButtons = document.querySelectorAll('.payment-field');
+    const paymentMethodValue = document.querySelector('#paymentMethod');
+
+    addListenersToButtons(shippingMethodButtons, shippingMethodValue);
+    addListenersToButtons(paymentMethodButtons, paymentMethodValue);
+}
+
+
+(async () => {
     document.addEventListener('click', handleClick);
+    initializeButtonListeners();
 
     fetchAndFillCountries();
+    await updateCountryByIP();
+    await updateCountryCode();
 })()
